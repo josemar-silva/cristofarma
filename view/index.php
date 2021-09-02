12770bb8b91f0ad425f05a9507990ee43ab60c2b
BrunoMikaelAlencarDePanha <86129331+BrunoMikaelAlencarDePanha@users.noreply.github.com>
2021-09-02 19:26:21 -0300

    Squashed commit of the following:
    commit 0178d6de803430d49688222078fe92f88106f1be
    Merge: 63b4c0d ac6cac4
    Author: BrunoMikaelAlencarDePanha <86129331+BrunoMikaelAlencarDePanha@users.noreply.github.com>
    Date:   Thu Sep 2 19:25:24 2021 -0300
    
        Merge branch 'master' into conteudo
    
    commit 63b4c0d076a5f895fba2d7b5281dc80302de33c3
    Author: BrunoMikaelAlencarDePanha <86129331+BrunoMikaelAlencarDePanha@users.noreply.github.com>
    Date:   Thu Sep 2 19:08:42 2021 -0300
    
        teste
    
    commit 9b38ee4ae1a9bf02a90b44ead67cecb02dc6b3e7
    Author: BrunoMikaelAlencarDePanha <86129331+BrunoMikaelAlencarDePanha@users.noreply.github.com>
    Date:   Wed Sep 1 23:01:43 2021 -0300
    
        Ajustes das telas de login
    
    commit 7dddf71f01a6e2282b6e31a9cb0e709a44c95ccf
    Author: BrunoMikaelAlencarDePanha <86129331+BrunoMikaelAlencarDePanha@users.noreply.github.com>
    Date:   Wed Sep 1 22:01:10 2021 -0300
    
        Mudança de pasta do bootstrap
    
    commit dce784d94da0033d3c43784bd1819e20deb5b61f
    Author: BrunoMikaelAlencarDePanha <86129331+BrunoMikaelAlencarDePanha@users.noreply.github.com>
    Date:   Wed Sep 1 21:37:10 2021 -0300
    
        Alteração de botões e imagem de fundo
    
    commit 6bb7d6d2cd6b8a271c542766f35e9e5e52347741
    Author: BrunoMikaelAlencarDePanha <86129331+BrunoMikaelAlencarDePanha@users.noreply.github.com>
    Date:   Wed Sep 1 21:05:19 2021 -0300
    
        css
    
    commit 9e1fd0d1dd51926ab7477875018e090e35342994
    Author: JjunioLima <89352831+JjunioLima@users.noreply.github.com>
    Date:   Sun Aug 29 21:18:54 2021 -0300
    
        ....

--- view/index.php
+++ view/index.php
@@ -5,23 +5,21 @@
 
 <head>
     <meta charset="UTF-8" />
-    <link rel="stylesheet" href="../css/estilo.css">
+    <link rel="stylesheet" href="../css/bootstrap/bootstrap.css">
     <title>Login</title>
     
 </head>
 
 
-<body class="index">   
+<body class="body">   
     <header>        
     </header>
     <section >
         <h1>Bem Vindo!</h1><br> 
         <h1>Qual o seu perfil de acesso?</h1><br/>
         <div>
-        <a href="LoginGerencial.php"><button style="background: #E0281D; border-radius: 6px; padding: 15px; cursor: pointer; color: #fff; border: none; font-size: 16px;">Login gerencial</button></a>
-        <a href="LoginUsuario.php"><button style="background: #E0281D; border-radius: 6px; padding: 15px; cursor: pointer; color: #fff; border: none; font-size: 16px;">Login usuario</button></a>
-        </div>       
-       
+        <a href="LoginGerencial.php"><button type="button" class="btn btn-outline-danger">Login gerencial</button></a>
+        <a href="LoginUsuario.php"><button type="button" class="btn btn-outline-danger">Login usuario</button></a>             
     </section>
 </body>
 
