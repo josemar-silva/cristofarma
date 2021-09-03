e96ac8ef18557c1a38499a5b923daaa6386c68d1
BrunoMikaelAlencarDePanha <86129331+BrunoMikaelAlencarDePanha@users.noreply.github.com>
2021-09-02 23:57:35 -0300

    Mudança dos links de navegaçao

--- view/home.php
+++ view/home.php
@@ -3,51 +3,46 @@
 
 <head>
     <meta charset="UTF-8" />
-    <link rel="stylesheet" href="../css/estilo.css">
+    <link rel="stylesheet" href="../css/bootstrap/nav/navegador.css">
     <title>Home</title>
 </head>
 
 <body>
     <header>
-
+    <ul class="nav nav-tabs">
+ 
+            <li class="nav-item">
+                <a class="nav-link" href="home.php">HOME</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="Pesquisar.php">PESQUISAR</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="Vendas.php">VENDAS</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="Caixa.php">CAIXA</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="CadastrarProdutos.php">PRODUTOS</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="CadastrarFornecedores.php">FORNECEDOR</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="CadastrarClientes.php">CLIENTES</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="CadastrarUsuarios.php">USUÁRIOS</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="NotaFiscal.php">NOTA FISCAL</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="Relatorios.php">RELATÓRIOS</a>
+            </li>
+    </ul>
     </header>
-    <section>
-        <section id="menu">
-            <p><a href="home.php">HOME</a></p>
-            <p><a href="Pesquisar.php">CONSULTAS</a></p>
-            <p><a href="Vendas.php">VENDAS</a></p>
-            <p><a href="Caixa.php">CAIXA</a></p>
-            <p><a href="CadastrarProdutos.php">PRODUTOS</a></p>
-            <p><a href="CadastrarFornecedores.php">FORNECEDOR</a></p>
-            <p><a href="CadastrarClientes.php">CLIENTES</a></p>
-            <p><a href="CadastrarUsuarios.php">USUÁRIOS</a></p>
-            <p><a href="NotaFiscal.php">NOTA FISCAL</a></p>
-            <p><a href="Relatorios.php">RELATÓRIO</a></p>
-        </section>
-        <section id="principal">
-            <p id="nomeEmpresa">DROGARIA CRISTOFARMA</p>
-            <fieldset id="missao">
-                <legend>
-                    <h2>Missão:</h1>
-                </legend>
-                <p>
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                    labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                    sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet,
-                    consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                    labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                    sunt in culpa qui officia deserunt mollit anim id est laborum."
-                </p>
-            </fieldset>
-            <div id="contatos">
-                <h1>Tele Entregas: (62) 3233-3333</h1>
-                <h1>WhatsApp: (62) 9 9999-9999</h1>
-            </div>
-        </section>
-</body>
+ </body>
 
 </html>
