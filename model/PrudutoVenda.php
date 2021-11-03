aa26fcfb4d51022d751439dda48916af655a4d8d
Josemar Silva <josemarsilvatxgyn@gmail.comm>
2021-11-03 01:24:13 -0300

    testes na session concluidos, insert e delete ok

--- model/PrudutoVenda.php
+++ model/PrudutoVenda.php
@@ -18,29 +18,28 @@ class ProdutoVenda
     
     }
 
-    public function createProdutoVenda($idVenda, $idProduto){
+    public function createProdutoVenda($venda_id_venda, $produto_id_produto){
 
         $conexao = new Conexao("projeto_cristofarma", "localhost", "root", "");
 
         $dados = $conexao->pdo->prepare("INSERT INTO produto_venda (venda_id_venda, produto_id_produto) VALUES (:idv, :idp)");
-        $dados->bindValue("idv", $idVenda);
-        $dados->bindValue("idp", $idProduto);
+        $dados->bindValue("idv", $venda_id_venda);
+        $dados->bindValue("idp", $produto_id_produto);
         $dados->execute();  
 
         return true;   
     }
 
-    public function deleteProdutoVenda($id_venda,$id_Produto){
+    public function deleteProdutoVenda($venda_id_venda, $produto_id_produto){
 
         $conexao = new Conexao("projeto_cristofarma", "localhost", "root", "");
 
         $dados = $conexao->pdo->prepare(" DELETE * produto_venda WHERE venda_id_venda = :idv AND produto_id_produto = :idp)");
-        $dados->bindValue("idv", $id_venda);
-        $dados->bindValue("idp", $id_Produto);
+        $dados->bindValue("idv", $venda_id_venda);
+        $dados->bindValue("idp", $produto_id_produto);
         $dados->execute();  
 
         return true;   
-        
     }
 
     public function updateProdutoVenda(){
