eb5b8b49eef9e989a6df6b62bd3988cf1dd96b21
BrunoMikaelAlencarDePanha <86129331+BrunoMikaelAlencarDePanha@users.noreply.github.com>
2021-12-09 20:07:08 -0300

    ...

--- view/ConsultaClientes.php
+++ view/ConsultaClientes.php
@@ -2,7 +2,7 @@
     require_once '../model/Pessoa.php';
 
     $pessoa = new Pessoa();
-    $usuarioLogado = $pessoa->login();              
+   // $usuarioLogado = $pessoa->login();              
 ?>
 
 <!doctype html>
